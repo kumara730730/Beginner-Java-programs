25ddec8de0643f6eb48b17af5bc86afd8ab58478
kumara730730 <kumara730730@gmail.com>
2025-11-09 22:51:40 +0530

    Implement basic GUI for user input and display

---
+++ basic_gui.java
@@ -0,0 +1,17 @@
+//Day 17
+import javax.swing.JOptionPane;
+class basic_gui
+{
+    public static void main(String args[])
+    {
+        String name = JOptionPane.showInputDialog("Enter your name");
+        JOptionPane.showMessageDialog(null, "Hello " + name);
+        
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+        JOptionPane.showMessageDialog(null, "Your age is " + age);
+        
+        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
+        JOptionPane.showMessageDialog(null, "Your height is " + height);
+        
+    }
+}
